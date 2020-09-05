cd75659ea749e636adf3e956c9347982fd177de4
rohitbari2000 <60724791+rohitbari2000@users.noreply.github.com>
2020-09-05 20:01:21 +0530

    Update script.js

--- assignment-4/script.js
+++ assignment-4/script.js
@@ -1,12 +1,12 @@
 (function () { 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 for (var i = 0; i < names.length; i++){
-  var firstLetter = names[i].charAt(0).toLowerCase();
+  var firstLetter = names[i].charAt().toLowerCase();
   if (firstLetter === 'j') { 
-byeSpeaker.speak(names[i]);
- } 
-else {
- helloSpeaker.speak(names[i]);
+    byeSpeaker.speak(names[i]);
+  } 
+  else {
+    helloSpeaker.speak(names[i]);
  }
 } 
 })();
